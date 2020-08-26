c76222b7770658d2a3f959fb7c02b1ef6ad682c8
nicklbaert <33283708+nicklbaert@users.noreply.github.com>
2020-08-26 18:22:40 +0200

    home page done

---
+++ src/components/footer.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="footer">
+    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="33.912" viewBox="0 0 177.5 33.912">
+      <g id="logo-svg" transform="translate(-278 -73)">
+        <g id="Gruppe_7" data-name="Gruppe 7" transform="translate(-28)">
+          <path
+            id="Pfad_7"
+            data-name="Pfad 7"
+            d="M13.356-17.82a5.267,5.267,0,0,0-1.584-.216,6.179,6.179,0,0,0-4.9,2.34V-17.82H2.412V0h4.86V-8.892c0-3.06,1.872-4.644,4.788-4.644h1.3ZM32.4-7.272a11.464,11.464,0,0,0,.324-2.664,8.108,8.108,0,0,0-8.388-8.28,9.191,9.191,0,0,0-9.324,9.324A9.341,9.341,0,0,0,24.768.432,10.9,10.9,0,0,0,31.9-2.232L29.88-5.544a8.035,8.035,0,0,1-4.968,1.8c-2.592,0-4.428-1.26-5-3.528Zm-12.492-3.42a4.389,4.389,0,0,1,4.248-3.6,3.626,3.626,0,0,1,3.852,3.6Zm31.536.108c0-4.932-2.988-7.632-7.632-7.632a9.717,9.717,0,0,0-7.776,3.672l2.52,2.988C40.14-13.248,41.436-14,43.272-14c2.16,0,3.312,1.152,3.312,3.132v.792H42.12c-4.14,0-6.732,1.98-6.732,5.184,0,3.132,2.376,5.328,6.156,5.328a7.424,7.424,0,0,0,5.436-2.16V0h4.464ZM46.584-6.66A3.543,3.543,0,0,1,42.84-3.132c-1.548,0-2.556-.756-2.556-1.944s.9-1.8,2.484-1.8h3.816ZM69.084-27v10.908a6.887,6.887,0,0,0-5.328-2.124c-4.968,0-8.64,3.96-8.64,9.324S58.788.432,63.756.432A6.78,6.78,0,0,0,69.48-2.2V0h4.464V-27Zm.072,18.108c0,3.2-2.052,4.9-4.5,4.9-2.628,0-4.608-1.944-4.608-4.932a4.6,4.6,0,0,1,4.608-4.9C67.1-13.824,69.156-12.06,69.156-8.892Zm25.992,1.62a11.463,11.463,0,0,0,.324-2.664,8.108,8.108,0,0,0-8.388-8.28A9.191,9.191,0,0,0,77.76-8.892,9.341,9.341,0,0,0,87.516.432a10.9,10.9,0,0,0,7.128-2.664L92.628-5.544a8.035,8.035,0,0,1-4.968,1.8c-2.592,0-4.428-1.26-5-3.528Zm-12.492-3.42a4.389,4.389,0,0,1,4.248-3.6,3.626,3.626,0,0,1,3.852,3.6ZM110.2-17.82a5.267,5.267,0,0,0-1.584-.216,6.179,6.179,0,0,0-4.9,2.34V-17.82H99.252V0h4.86V-8.892c0-3.06,1.872-4.644,4.788-4.644h1.3Zm15.192,0-4.644,11.7-4.356-11.7H111.24l6.84,16.74L117.54.144a3.547,3.547,0,0,1-3.708,2.412c-.288,0-.972-.036-2.088-.144V6.48a12.14,12.14,0,0,0,3.132.432c3.312,0,5.472-1.656,7.092-5.364L130.5-17.82Z"
+            transform="translate(353 100)"
+            fill="#ffffff"
+          />
+          <g
+            id="Icon_ionic-ios-book"
+            data-name="Icon ionic-ios-book"
+            transform="translate(302.618 73.5)"
+          >
+            <path
+              id="Pfad_1"
+              data-name="Pfad 1"
+              d="M11.895,4.507c4.7,0,8.7,1.962,8.7,5.766V11.4h0V31.359c0,.155-.27.19-.33.049V31.4A7.846,7.846,0,0,0,13.1,26.445a12.548,12.548,0,0,0-8.1,2.609,1.054,1.054,0,0,1-.574.2H4.043c-.322,0-.661-.19-.661-.429V8.452C3.373,6.195,7.191,4.507,11.895,4.507Z"
+              fill="#46ccff"
+            />
+            <path
+              id="Pfad_2"
+              data-name="Pfad 2"
+              d="M27.4,4.5c-4.7,0-8.7,1.962-8.7,5.766v1.125h0V31.352c0,.155.27.19.33.049v-.007a7.935,7.935,0,0,1,7.156-4.957,12.546,12.546,0,0,1,8.1,2.609,1.113,1.113,0,0,0,.574.2h.391c.322,0,.661-.19.661-.429V8.438C35.92,6.188,32.1,4.5,27.4,4.5Z"
+              transform="translate(3.626)"
+              fill="#46ccff"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+    <div id="footer-content">
+      <table id="footer-links">
+        <tr>
+          <td>
+            <router-link to="/">Home</router-link>
+          </td>
+          <td>
+            <router-link to="/about">Privacy</router-link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <router-link to="/about">How it works</router-link>
+          </td>
+          <td>
+            <router-link to="/plans">Terms of Service</router-link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <router-link to="/plans">Plans</router-link>
+          </td>
+          <td>
+            <router-link to="/login">Imprint</router-link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <router-link to="/login">Login</router-link>
+          </td>
+        </tr>
+      </table>
+      <div id="newsletter-wrapper">
+        <input
+          type="text"
+          placeholder="Enter your email address to sign up for our newsletter"
+          id="newsletter-input"
+        />
+        <button id="newsletter-send">Send</button>
+      </div>
+    </div>
+    <div id="footer-bottom">
+      <h3>© Readery GmbH 2020</h3>
+      <div id="social-media">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" viewBox="0 0 81.577 81.558">
+          <path
+            id="Icon_awesome-instagram"
+            data-name="Icon awesome-instagram"
+            d="M40.792,22.106A20.911,20.911,0,1,0,61.7,43.017,20.877,20.877,0,0,0,40.792,22.106Zm0,34.505A13.595,13.595,0,1,1,54.387,43.017,13.619,13.619,0,0,1,40.792,56.611Zm26.643-35.36a4.877,4.877,0,1,1-4.877-4.877A4.866,4.866,0,0,1,67.435,21.251ZM81.285,26.2c-.309-6.533-1.8-12.321-6.588-17.089-4.768-4.768-10.555-6.26-17.089-6.588-6.734-.382-26.916-.382-33.65,0-6.515.309-12.3,1.8-17.089,6.57S.609,19.649.281,26.183C-.1,32.916-.1,53.1.281,59.833c.309,6.533,1.8,12.321,6.588,17.089s10.555,6.26,17.089,6.588c6.734.382,26.916.382,33.65,0,6.533-.309,12.321-1.8,17.089-6.588,4.768-4.768,6.26-10.555,6.588-17.089.382-6.734.382-26.9,0-33.632Zm-8.7,40.857a13.763,13.763,0,0,1-7.753,7.753c-5.369,2.129-18.108,1.638-24.041,1.638s-18.69.473-24.041-1.638A13.763,13.763,0,0,1,9,67.058C6.869,61.689,7.361,48.95,7.361,43.017S6.888,24.327,9,18.976a13.763,13.763,0,0,1,7.753-7.753C22.12,9.094,34.859,9.585,40.792,9.585s18.69-.473,24.041,1.638a13.763,13.763,0,0,1,7.753,7.753c2.129,5.369,1.638,18.108,1.638,24.041S74.715,61.707,72.586,67.058Z"
+            transform="translate(0.005 -2.238)"
+            fill="#D0DBE8"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100.221"
+          height="20"
+          viewBox="0 0 100.221 81.398"
+        >
+          <path
+            id="Icon_awesome-twitter"
+            data-name="Icon awesome-twitter"
+            d="M89.92,23.667c.064.89.064,1.781.064,2.671,0,27.154-20.667,58.441-58.441,58.441A58.045,58.045,0,0,1,0,75.558a42.492,42.492,0,0,0,4.96.254,41.136,41.136,0,0,0,25.5-8.776A20.577,20.577,0,0,1,11.256,52.792a25.9,25.9,0,0,0,3.879.318,21.724,21.724,0,0,0,5.405-.7A20.543,20.543,0,0,1,4.07,32.252V32A20.686,20.686,0,0,0,13.354,34.6,20.571,20.571,0,0,1,6.995,7.133,58.385,58.385,0,0,0,49.348,28.627a23.188,23.188,0,0,1-.509-4.706A20.56,20.56,0,0,1,84.387,9.867a40.44,40.44,0,0,0,13.036-4.96,20.485,20.485,0,0,1-9.03,11.319,41.177,41.177,0,0,0,11.828-3.179,44.154,44.154,0,0,1-10.3,10.62Z"
+            transform="translate(0 -3.381)"
+            fill="#D0DBE8"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="43.757"
+          height="20"
+          viewBox="0 0 43.757 81.699"
+        >
+          <path
+            id="Icon_awesome-facebook-f"
+            data-name="Icon awesome-facebook-f"
+            d="M42.5,45.956,44.768,31.17H30.581V21.575c0-4.045,1.982-7.988,8.336-7.988h6.45V1A78.653,78.653,0,0,0,33.917,0C22.234,0,14.6,7.082,14.6,19.9V31.17H1.609V45.956H14.6V81.7H30.581V45.956Z"
+            transform="translate(-1.609)"
+            fill="#D0DBE8"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  components: {}
+};
+</script>
+
+<style scoped>
+.footer {
+  height: 400px;
+  background-color: #1b1f23;
+  padding: 0 15%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: center;
+}
+#footer-content {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#footer-links {
+  margin-top: 2%;
+}
+#footer-links td {
+  width: 200px;
+  text-align: left;
+}
+#footer-links tr {
+  height: 35px;
+}
+#footer-links a {
+  margin: 20px 0;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #d0dbe8;
+  font-weight: 400;
+  font-size: 14px;
+  letter-spacing: 1px;
+}
+
+#newsletter-wrapper {
+  height: 60px;
+  width: 50%;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+#newsletter-wrapper input {
+  height: 100%;
+  border: none;
+  width: 70%;
+  font-family: "Cera Pro", Helvetica, sans-serif;
+  font-size: 16px;
+  padding: 0 15px;
+  font-weight: 300;
+  outline: none;
+}
+#newsletter-wrapper input:focus {
+  border: none;
+  outline: none;
+}
+#newsletter-wrapper button {
+  height: 100%;
+  border: none;
+  padding: 0;
+  width: 25%;
+  background-color: #40caff;
+  color: #fff;
+  font-family: "Cera Pro", Helvetica, sans-serif;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.2s ease-in;
+}
+#newsletter-wrapper button:hover {
+  border: none;
+  outline: none;
+  background-color: #1ab5f1;
+  transition: 0.2s ease-out;
+}
+#newsletter-wrapper button:focus {
+  border: none;
+  outline: none;
+}
+#footer-bottom {
+  margin-top: 3%;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+#footer-bottom h3 {
+  color: #d0dbe8;
+  font-size: 14px;
+  font-weight: 300;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,13 +8,83 @@
       Start your reading journey today with readery.
       <br />Select your interests and we'll take care of the rest.
     </h2>
+    <div id="action-buttons">
+      <a href="#" id="learn-more-button">Learn More</a>
+      <a href="#" id="get-started-button">Get Started</a>
+    </div>
+    <div id="image-1-wrapper">
+      <h2 id="newsletter-heading">Want 30% off?</h2>
+      <h3 id="newsletter-subheading">join our newsletter and get 30% off on all plans</h3>
+      <div id="newsletter-input-wrapper">
+        <input type="text" placeholder="Enter your E-Mail address" id="newsletter-input" />
+        <button id="newsletter-send">Send</button>
+      </div>
+    </div>
+    <div class="step" id="step-1">
+      <div class="step-content" id="step-content-1">
+        <h4 class="step-number">STEP 1</h4>
+        <h2 class="step-heading">
+          You tell us what you
+          are interested in
+        </h2>
+        <h3 class="step-subheading">
+          Select from over 50 different categories,
+          which ones you like the most
+        </h3>
+      </div>
+      <div class="step-image" id="step-image-1"></div>
+    </div>
+    <div class="step" id="step-2">
+      <div class="step-content" id="step-content-2">
+        <h4 class="step-number">STEP 2</h4>
+        <h2 class="step-heading">
+          Lean back and
+          let us do the rest
+        </h2>
+        <h3 class="step-subheading">
+          Readery plots out your reading journey
+          by selecting the best books for your
+          preferences
+        </h3>
+      </div>
+      <div class="step-image" id="step-image-2"></div>
+    </div>
+    <div class="step" id="step-3">
+      <div class="step-content" id="step-content-3">
+        <h4 class="step-number">STEP 3</h4>
+        <h2 class="step-heading">
+          We send you a new
+          book each month
+        </h2>
+        <h3 class="step-subheading">
+          Lean back and start learning. When you're
+          finished, the next book is already waiting to
+          be shipped to your doorstep
+        </h3>
+      </div>
+      <div class="step-image" id="step-image-3"></div>
+    </div>
+    <div class="step" id="step-4">
+      <div class="step-content" id="step-content-4">
+        <h4 class="step-number">STEP 4</h4>
+        <h2 class="step-heading">You are in full control</h2>
+        <h3
+          class="step-subheading"
+        >Skip a month, swap out a book you've already read or cancel your subscription at any time online</h3>
+      </div>
+      <div class="step-image" id="step-image-4"></div>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from "../components/footer.vue";
 export default {
   name: "Home",
-  components: {}
+  components: {
+    Footer
+  }
 };
 </script>
 
@@ -28,7 +98,155 @@ export default {
   font-size: 80px;
 }
 #subheading {
-  margin-top: 2%;
+  margin-top: 3%;
   font-weight: 300;
+}
+#action-buttons {
+  margin-top: 3%;
+  width: 100%;
+  height: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#action-buttons a {
+  margin: 0 1em;
+}
+#learn-more-button {
+  background-color: #fff;
+  padding: 15px 30px;
+  color: #1b1f23;
+  text-decoration: none;
+  box-shadow: inset 0px 0px 0px 2px #1b1f23;
+  transition: 0.2s ease-in;
+}
+#learn-more-button:hover {
+  transition: 0.2s ease-out;
+}
+#get-started-button {
+  background-color: #40caff;
+  padding: 15px 30px;
+  color: #fff;
+  text-decoration: none;
+  transition: 0.2s ease-in;
+}
+#get-started-button:hover {
+  background-color: #1ab5f1;
+  transition: 0.2s ease-out;
+}
+#image-1-wrapper {
+  margin-top: 5%;
+  width: 100%;
+  height: 500px;
+  background-image: url("../assets/images/man-window-reading.png");
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+#newsletter-heading {
+  font-size: 50px;
+}
+#newsletter-subheading {
+  margin-top: 0.5%;
+  font-weight: 300;
+  font-size: 20px;
+}
+#newsletter-input-wrapper {
+  margin-top: 2%;
+  width: 40vw;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+#newsletter-input-wrapper input {
+  height: 100%;
+  border: none;
+  width: 75%;
+  font-family: "Cera Pro", Helvetica, sans-serif;
+  font-size: 16px;
+  padding: 0 15px;
+  font-weight: 300;
+  outline: none;
+}
+#newsletter-input-wrapper input:focus {
+  border: none;
+  outline: none;
+}
+#newsletter-input-wrapper button {
+  height: 100%;
+  border: none;
+  padding: 0;
+  width: 20%;
+  background-color: #40caff;
+  color: #fff;
+  font-family: "Cera Pro", Helvetica, sans-serif;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.2s ease-in;
+}
+#newsletter-input-wrapper button:hover {
+  border: none;
+  outline: none;
+  background-color: #1ab5f1;
+  transition: 0.2s ease-out;
+}
+#newsletter-input-wrapper button:focus {
+  border: none;
+  outline: none;
+}
+
+.step {
+  margin: 10% 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 15%;
+  flex-wrap: wrap;
+}
+.step-content {
+  width: 40%;
+}
+.step-heading {
+  margin-top: 2%;
+  font-size: 50px;
+  text-align: left;
+}
+.step-subheading {
+  margin-top: 5%;
+  font-size: 20px;
+  text-align: left;
+  font-weight: 300;
+}
+
+.step-number {
+  text-align: left;
+  font-weight: 700;
+  color: #aab7c6;
+  font-size: 20px;
+}
+.step-image {
+  height: 400px;
+  width: 55%;
+  background-position: center;
+}
+#step-2,
+#step-4 {
+  flex-direction: row-reverse;
+}
+#step-image-1 {
+  background-image: url("../assets/images/interest-demo.png");
+}
+#step-image-2 {
+  background-image: url("../assets/images/man-chill.png");
+}
+#step-image-3 {
+  background-image: url("../assets/images/plan-demo.png");
+}
+#step-image-4 {
+  background-image: url("../assets/images/girl-laptop-2.png");
 }
 </style>
