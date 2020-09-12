3c3dbf9978cd059bb4cfd68491e6359299ba51c2
nicklbaert <niclasillg@gmail.com>
2020-09-12 16:11:11 +0200

    analytics integration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueAnalytics from 'vue-analytics';
 import AxiosPlugin from 'vue-axios-cors';
  
 Vue.use(AxiosPlugin);
+Vue.use(VueAnalytics, {
+    id: 'UA-117438770-2',
+    router
+}
 
 
 
